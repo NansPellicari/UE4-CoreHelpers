5525be03171ddc2d7f739c8e24b0e9a1a74408c8
Nans Pellicari <nans.pellicari@gmail.com>
2020-05-30 18:24:23 +0200

    some fixes + allow Assertions working for GG tests

--- Source/NansCoreHelpers/Public/Misc/NansAssertionMacros.h
+++ Source/NansCoreHelpers/Public/Misc/NansAssertionMacros.h
@@ -2,35 +2,35 @@
 
 #include "CoreMinimal.h"
 
-#if WITH_DEV_AUTOMATION_TESTS
+#if WITH_DEV_AUTOMATION_TESTS || defined WITH_GG_TESTS
 
 extern NANSCOREHELPERS_API bool GNAssertThrowError;
 
 #define mycheck(expr)          \
-    {                          \
-        if (UNLIKELY(!(expr))) \
-        {                      \
-            throw TEXT(#expr); \
-        }                      \
-    }
+	{                          \
+		if (UNLIKELY(!(expr))) \
+		{                      \
+			throw TEXT(#expr); \
+		}                      \
+	}
 #define mycheckf(expr, format, ...) AssertHelpers::LogAndThrow(!!(expr), TEXT(#expr), format, ##__VA_ARGS__)
 
 #define myensureMsgf(InExpression, InFormat, ...) \
-    (AssertHelpers::LogAndThrow(!!(InExpression), TEXT(#InExpression), InFormat, ##__VA_ARGS__))
+	(AssertHelpers::LogAndThrow(!!(InExpression), TEXT(#InExpression), InFormat, ##__VA_ARGS__))
 
 namespace AssertHelpers
 {
-    template <typename ExprType, typename FmtType, typename... Types>
-    inline bool LogAndThrow(const bool Result, const ExprType& Expr, const FmtType& Format, Types... Args)
-    {
-        if (!Result)
-        {
-            UE_LOG(LogTemp, Display, Format, Args...);
-            if (GNAssertThrowError) throw Expr;
-        }
-        return Result;
-    }
-}    // namespace AssertHelpers
+	template <typename ExprType, typename FmtType, typename... Types>
+	inline bool LogAndThrow(const bool Result, const ExprType& Expr, const FmtType& Format, Types... Args)
+	{
+		if (!Result)
+		{
+			UE_LOG(LogTemp, Display, Format, Args...);
+			if (GNAssertThrowError) throw Expr;
+		}
+		return Result;
+	}
+}	 // namespace AssertHelpers
 #else
 #define mycheck(expr) check(expr)
 #define mycheckf(expr, format, ...) checkf(expr, format, ##__VA_ARGS__)
