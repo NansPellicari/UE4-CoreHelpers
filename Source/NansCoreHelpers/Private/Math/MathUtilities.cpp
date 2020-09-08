17a5ed3eaca073ca7ec88b0af7573e4d93d1ba2f
Nans Pellicari <nans.pellicari@gmail.com>
2020-09-09 01:11:08 +0200

    improve: ZoneBox + Add basics fns for collisions

--- Source/NansCoreHelpers/Private/Math/MathUtilities.cpp
+++ Source/NansCoreHelpers/Private/Math/MathUtilities.cpp
@@ -9,16 +9,20 @@ FZoneBox::FZoneBox()
 
 FBox FZoneBox::GetBox() const
 {
-	// return UMathExtensionLibrary::CreateBoxFromInitAndExtend(Origin, Extent, FRotator::ZeroRotator);
 	return FBox(Origin - Extent, Origin + Extent);
+}
+FBox FZoneBox::GetBoundingBox() const
+{
+	FOrientedBox OBB = GetOrientedBox();
+	FVector Vertices[8];
+	OBB.CalcVertices(Vertices);
+	FBox Box = FBox(Vertices, 8);
+	return Box;
 }
 
 FSphere FZoneBox::GetSphere() const
 {
-	float W = (Origin - (Origin + Extent)).Size();
-	FSphere Sphere(Origin, W);
-
-	return Sphere;
+	return FSphere(Origin, Extent.GetMax());
 }
 FSphere FZoneBox::GetSphereXY() const
 {
@@ -30,15 +34,99 @@ FSphere FZoneBox::GetSphereXY() const
 	return Sphere;
 }
 
+FOrientedBox FZoneBox::GetOrientedBox() const
+{
+	return NGeometry::CreateOrientedBox(GetBox(), Rotation);
+}
+
 bool FZoneBox::Intersect(const FZoneBox& Other) const
 {
-	bool bIsIntersect = false;
-	if (GetBox().Intersect(Other.GetBox()))
+	// Sphere is the bigger volume, so it's require to test it first
+	if (Other.CollideOn == EZoneCollider::Sphere)
 	{
-		bIsIntersect = GetSphereXY().Intersects(Other.GetSphereXY());
+		return Intersect(Other.GetSphere());
 	}
 
-	return bIsIntersect;
+	// Try first the Bounding box cause all other volumes are inner bounding box
+	if (!GetBoundingBox().Intersect(Other.GetBoundingBox())) return false;
+
+	if (Other.CollideOn == EZoneCollider::AAB)
+	{
+		return Intersect(Other.GetBox());
+	}
+	if (Other.CollideOn == EZoneCollider::OBB)
+	{
+		return Intersect(Other.GetOrientedBox());
+	}
+	if (Other.CollideOn == EZoneCollider::SphereXY)
+	{
+		return Intersect(Other.GetSphereXY());
+	}
+
+	return false;
+}
+
+bool FZoneBox::Intersect(const FSphere& Other) const
+{
+	if (CollideOn == EZoneCollider::Sphere)
+	{
+		return GetSphere().Intersects(Other);
+	}
+	if (CollideOn == EZoneCollider::SphereXY)
+	{
+		return GetSphereXY().Intersects(Other);
+	}
+	if (CollideOn == EZoneCollider::AAB)
+	{
+		return FMath::SphereAABBIntersection(Other, GetBox());
+	}
+	if (CollideOn == EZoneCollider::OBB)
+	{
+		return NGeometry::Intersects(GetOrientedBox(), Other);
+	}
+	return false;
+}
+
+bool FZoneBox::Intersect(const FBox& Other) const
+{
+	if (CollideOn == EZoneCollider::Sphere)
+	{
+		return FMath::SphereAABBIntersection(GetSphere(), Other);
+	}
+	if (CollideOn == EZoneCollider::SphereXY)
+	{
+		return FMath::SphereAABBIntersection(GetSphereXY(), Other);
+	}
+	if (CollideOn == EZoneCollider::AAB)
+	{
+		return GetBox().Intersect(Other);
+	}
+	if (CollideOn == EZoneCollider::OBB)
+	{
+		return NGeometry::Intersects(Other, GetOrientedBox());
+	}
+	return false;
+}
+
+bool FZoneBox::Intersect(const FOrientedBox& Other) const
+{
+	if (CollideOn == EZoneCollider::Sphere)
+	{
+		return NGeometry::Intersects(Other, GetSphere());
+	}
+	if (CollideOn == EZoneCollider::SphereXY)
+	{
+		return NGeometry::Intersects(Other, GetSphereXY());
+	}
+	if (CollideOn == EZoneCollider::AAB)
+	{
+		return NGeometry::Intersects(GetBox(), Other);
+	}
+	if (CollideOn == EZoneCollider::OBB)
+	{
+		return NGeometry::Intersects(GetOrientedBox(), Other);
+	}
+	return false;
 }
 
 FVector NMathUtilities::GetClosestCorner(
@@ -68,3 +156,147 @@ FVector NMathUtilities::GetClosestCorner(
 	}
 	return ClosestCorner;
 };
+
+bool NGeometry::Intersects(const FBox& AABB, const FOrientedBox& OBB)
+{
+	// Thanks to this: https://github.com/gszauer/GamePhysicsCookbook/blob/master/Code/Geometry3D.cpp
+	FVector O[8];
+	OBB.CalcVertices(O);
+
+	FVector Test[15] = {FVector(1, 0, 0), FVector(0, 1, 0), FVector(0, 0, 1), O[0], O[1], O[2]};
+
+	for (int I = 0; I < 3; ++I)
+	{	 // Fill out rest of axis
+		Test[6 + I * 3 + 0] = FVector::CrossProduct(Test[I], Test[0]);
+		Test[6 + I * 3 + 1] = FVector::CrossProduct(Test[I], Test[1]);
+		Test[6 + I * 3 + 2] = FVector::CrossProduct(Test[I], Test[2]);
+	}
+
+	for (int I = 0; I < 15; ++I)
+	{
+		if (!NGeometry::OverlapOnAxis(AABB, OBB, Test[I]))
+		{
+			return false;	 // Seperating axis found
+		}
+	}
+
+	return false;
+}
+
+FVector NGeometry::ClosestPoint(const FOrientedBox& OBB, const FVector& Point)
+{
+	FVector Result = OBB.Center;
+	FVector Dir = Point - OBB.Center;
+	float Size[3] = {OBB.ExtentX, OBB.ExtentY, OBB.ExtentZ};
+	FVector Axis[3] = {OBB.AxisX, OBB.AxisY, OBB.AxisZ};
+
+	for (int i = 0; i < 3; ++i)
+	{
+		float Distance = FVector::DotProduct(Dir, Axis[i]);
+
+		if (Distance > Size[i])
+		{
+			Distance = Size[i];
+		}
+		if (Distance < -Size[i])
+		{
+			Distance = -Size[i];
+		}
+
+		Result = Result + (Axis[i] * Distance);
+	}
+
+	return Result;
+}
+
+bool NGeometry::Intersects(const FOrientedBox& OBB, const FSphere& Sphere)
+{
+	FVector ClosestPoint = NGeometry::ClosestPoint(OBB, Sphere.Center);
+	float DistSq = (Sphere.Center - ClosestPoint).SizeSquared();
+	float RadiusSq = Sphere.W * Sphere.W;
+	return DistSq < RadiusSq;
+}
+
+bool NGeometry::Intersects(const FOrientedBox& OBB1, const FOrientedBox& OBB2)
+{
+	FVector O1[8];
+	FVector O2[8];
+	OBB1.CalcVertices(O1);
+	OBB2.CalcVertices(O2);
+
+	FVector Test[15] = {O1[0], O1[1], O1[2], O2[0], O2[1], O2[2]};
+
+	// Or test this:
+	// // // FVector Test[15] = {OBB1.AxisX, OBB1.AxisY, OBB1.AxisZ, OBB2.AxisX, OBB2.AxisY, OBB2.AxisZ};
+
+	for (int I = 0; I < 3; ++I)
+	{	 // Fill out rest of axis
+		Test[6 + I * 3 + 0] = FVector::CrossProduct(Test[I], Test[0]);
+		Test[6 + I * 3 + 1] = FVector::CrossProduct(Test[I], Test[1]);
+		Test[6 + I * 3 + 2] = FVector::CrossProduct(Test[I], Test[2]);
+	}
+
+	for (int I = 0; I < 15; ++I)
+	{
+		if (!NGeometry::OverlapOnAxis(OBB1, OBB2, Test[I]))
+		{
+			return false;	 // Seperating axis found
+		}
+	}
+
+	return false;
+}
+
+bool NGeometry::OverlapOnAxis(const FOrientedBox& OBB1, const FOrientedBox& OBB2, const FVector& Axis)
+{
+	FFloatInterval A = OBB1.Project(Axis);
+	FFloatInterval B = OBB2.Project(Axis);
+	return ((B.Min <= A.Max) && (A.Min <= B.Max));
+}
+
+bool NGeometry::OverlapOnAxis(const FBox& AABB, const FOrientedBox& OBB, const FVector& Axis)
+{
+	FFloatInterval A = NGeometry::GetInterval(AABB, Axis);
+	FFloatInterval B = OBB.Project(Axis);
+	return ((B.Min <= A.Max) && (A.Min <= B.Max));
+}
+
+FFloatInterval NGeometry::GetInterval(const FBox& AABB, const FVector& Axis)
+{
+	TArray<FVector> Vertices = NGeometry::GetVertices(AABB);
+	FFloatInterval ProjectionInterval;
+
+	for (int i = 1; i < 8; ++i)
+	{
+		ProjectionInterval.Include(FVector::DotProduct(Axis, Vertices[i]));
+	}
+
+	return ProjectionInterval;
+}
+
+TArray<FVector> NGeometry::GetVertices(const FBox& AABB)
+{
+	TArray<FVector> Vertices = {FVector(AABB.Min),
+		FVector(AABB.Min.X, AABB.Min.Y, AABB.Max.Z),
+		FVector(AABB.Min.X, AABB.Max.Y, AABB.Min.Z),
+		FVector(AABB.Max.X, AABB.Min.Y, AABB.Min.Z),
+		FVector(AABB.Max.X, AABB.Max.Y, AABB.Min.Z),
+		FVector(AABB.Max.X, AABB.Min.Y, AABB.Max.Z),
+		FVector(AABB.Min.X, AABB.Max.Y, AABB.Max.Z),
+		FVector(AABB.Max)};
+	return Vertices;
+}
+
+FOrientedBox NGeometry::CreateOrientedBox(const FBox& AABB, const FRotator& Rot)
+{
+	FOrientedBox OBB;
+	OBB.Center = AABB.GetCenter();
+	OBB.ExtentX = AABB.GetExtent().X;
+	OBB.ExtentY = AABB.GetExtent().Y;
+	OBB.ExtentZ = AABB.GetExtent().Z;
+	FQuat Quat = Rot.Quaternion();
+	OBB.AxisX = Quat.GetAxisX();
+	OBB.AxisY = Quat.GetAxisY();
+	OBB.AxisZ = Quat.GetAxisZ();
+	return OBB;
+}
--- Source/NansCoreHelpers/Public/Math/MathUtilities.h
+++ Source/NansCoreHelpers/Public/Math/MathUtilities.h
@@ -17,6 +17,16 @@ enum ELateralityOrientation
 
 ENUM_CLASS_FLAGS(ELateralityOrientation)
 
+UENUM(BlueprintType)
+enum class EZoneCollider : uint8
+{
+	AABB UMETA(DisplayName = "Bounding Box"),
+	AAB UMETA(DisplayName = "Box"),
+	OBB UMETA(DisplayName = "Oriented Box"),
+	Sphere UMETA(DisplayName = "Sphere"),
+	SphereXY UMETA(DisplayName = "Sphere XY"),
+};
+
 USTRUCT(BlueprintType)
 struct NANSCOREHELPERS_API FZoneBox
 {
@@ -31,14 +41,27 @@ struct NANSCOREHELPERS_API FZoneBox
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ZoneBox|Settings")
 	FRotator Rotation;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ZoneBox|Settings")
+	EZoneCollider CollideOn = EZoneCollider::SphereXY;
+
 	FZoneBox();
 
 	FZoneBox(FVector _Origin, FVector _Extent, FRotator _Rotation) : Origin(_Origin), Extent(_Extent), Rotation(_Rotation) {}
 
+	/** Only returns the FBox (AAB) build with origin and extend */
 	FBox GetBox() const;
+	/** Returns the FBox (AABB) build with origin and extend + rotation */
+	FBox GetBoundingBox() const;
+	/** Returns the FOrientedBox (OBB) build with origin and extend + rotation */
+	FOrientedBox GetOrientedBox() const;
+	/** Returns the FSphere which wraps everything */
 	FSphere GetSphere() const;
+	/** Returns the FSphere inscribed (in the X & Y axis only) on the AAB box */
 	FSphere GetSphereXY() const;
 	bool Intersect(const FZoneBox& Other) const;
+	bool Intersect(const FSphere& Other) const;
+	bool Intersect(const FBox& Other) const;
+	bool Intersect(const FOrientedBox& Other) const;
 };
 
 // 0: Left corner, 1: right corner
@@ -49,4 +72,21 @@ class NANSCOREHELPERS_API NMathUtilities
 public:
 	static FVector GetClosestCorner(
 		int32 SideToTest, const FZoneBox& Box, const TCorners& CornerData, ELateralityOrientation& Side, bool bDebug = false);
+};
+
+/**
+ *  this class is widely inspired by https://github.com/gszauer/GamePhysicsCookbook/blob/master/Code/Geometry3D.cpp
+ */
+class NANSCOREHELPERS_API NGeometry
+{
+public:
+	static FOrientedBox CreateOrientedBox(const FBox& AABB, const FRotator& Rot);
+	static bool Intersects(const FBox& AABB, const FOrientedBox& OBB);
+	static bool Intersects(const FOrientedBox& OBB, const FSphere& Sphere);
+	static bool Intersects(const FOrientedBox& OBB1, const FOrientedBox& OBB2);
+	static bool OverlapOnAxis(const FOrientedBox& OBB1, const FOrientedBox& OBB2, const FVector& axis);
+	static bool OverlapOnAxis(const FBox& AABB, const FOrientedBox& OBB, const FVector& axis);
+	static FFloatInterval GetInterval(const FBox& AABB, const FVector& axis);
+	static TArray<FVector> GetVertices(const FBox& AABB);
+	static FVector ClosestPoint(const FOrientedBox& OBB, const FVector& Point);
 };
